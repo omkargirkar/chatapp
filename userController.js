d30df730419dade0f86885c967ed5e3ad79bb29b
omkargirkar <omkar.girkar18@gmail.com>
2025-06-07 23:20:33 +0530

    login backend

--- userController.js
+++ userController.js
@@ -1,5 +1,6 @@
 const db = require('../db');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.addUser = async (req, res) => {
   
@@ -21,8 +22,48 @@ exports.addUser = async (req, res) => {
     });
   } catch (err) {
     if (err.code === 'ER_DUP_ENTRY') {
-      return res.status(403).json({ error: 'Email already exists.' });
+      return res.status(403).json({ error: 'User already exists. Please Login.' });
     }
     res.status(500).json({ error: 'Failed to store user data' });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const [userResult] = await db.execute(
+      'SELECT * FROM users WHERE email = ?',
+      [email]
+    );
+
+    // If no user with the given email
+    if (userResult.length === 0) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const user = userResult[0];
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Password incorrect. User not authorized.' });
+    }
+
+    // If both email and password match, token created
+
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      'your_jwt_secret_key',
+      { expiresIn: '1h' }
+    );
+    
+    res.status(200).json({
+      message: 'User login successful',
+      token: token,
+      user: { id: user.id, username: user.username, email: user.email }
+    });
+  } catch (err) {
+    res.status(500).json({ error: 'Login failed due to server error' });
   }
 };
